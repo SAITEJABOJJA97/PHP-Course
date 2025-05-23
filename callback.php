cdd0678145384be70d45758651e14c5e49bdb0e3
SAITEJABOJJA97 <sait.bojja@gmail.com>
2025-05-23 18:49:42 +0100

    23/05/25 made two ways of callback

--- callback.php
+++ callback.php
@@ -1,18 +1,18 @@
 <?php
 // this is call back function 
-function demo_callback($data) {
+// function demo_callback($data) {
 
-    return strlen($data);
-    //return $data;
+//     //return strlen($data);
+//     return $data;
 
 
-}
+// }
 
 
 $sdata = ["apple","banana","pineapple","mengo"];
 $qdata = ["audi","duster","bmw","mini"];
 
-$len = array_map("demo_callback",$sdata);
+//$len = array_map("demo_callback",$sdata);
 $len2 = array_map( function($data) {
 
     //return strlen($data);
@@ -22,7 +22,7 @@ $len2 = array_map( function($data) {
 },$qdata);
 
 
-echo '<pre>'.print_r($len,true).'</pre>';
+//echo '<pre>'.print_r($len,true).'</pre>';
 echo '<pre>'.print_r($len2,true).'</pre>';
 
 ?>
