55db9cce7b55b4fc14a110403d0aa7f159edd509
SAITEJABOJJA97 <sait.bojja@gmail.com>
2025-06-06 17:53:04 +0100

    Create sai_teja_php.php

---
+++ sai_teja_php.php
@@ -0,0 +1,91 @@
+
+
+<html>
+    <body>
+        <!-- <form action="sai_teja_php.php" method="POST">
+            Name :<input type="text" name="name"><br>
+            age :<input type="number" name="age"><br>
+            password : <input type="password" name="password"><br>
+            <input type="submit">
+        </form><br> -->
+        <!-- <form action="sai_teja_php.php" method="get">
+            <input type="number" name="num1">
+            <br>
+            <input type="number" name="num2">
+            <input type="submit">
+        </form>-->
+        <!-- <form action="sai_teja_php.php" method="get">
+            color : <input type="text" name="color"> <br>
+            pular noun : <input type="text" name="pluralnoun"> <br>
+            celebrity : <input type="text" name="celebrity"> <br>
+            <input type="submit">
+        </form> -->
+        <!-- <form action="sai_teja_php.php" method="POST">
+            apples :  <input type="checkbox" name="fruit[]" value="apples"><br>
+            oranges :  <input type="checkbox" name="fruit[]" value="oranges"><br>
+            peach :  <input type="checkbox" name="fruit[]" value="peach"><br>
+            <input type="submit">
+        </form> -->
+        <form action="sai_teja_php.php" method="POST">
+             <input type="text" name="student">
+             <input type="submit">
+             </form>
+        <?php
+
+        // echo("hello world");
+
+        // $name = "sai";
+        // $age = 26;
+        // echo ("<br> my name is $name </br>");
+        // echo ("my age is $age </br>");
+        // echo ("he liked the name $name </br>");
+        // echo ("but dont like age $age </br>");
+
+        // $name1 = "karthik bojja";
+        // echo strtoupper ($name1 . "</br>");
+        // echo strlen ($name1 . "</br>");
+        // echo "<br>" . $name1[3] . "</br>";
+        // $name1[0]= "B";
+        // echo ($name1 . "</br>");
+        // echo str_replace("bojja","lingala",$name1 . "</br>");
+        // echo substr($name1, 9,2);
+        
+        // echo "<br>" . $_POST["name"] . "</br>";
+        // echo $_POST["age"] . "</br>";
+        // echo $_POST["password"];
+        // echo "<br>" . $_GET["num1"] + $_GET["num2"] . "</br>";
+        // $color =$_GET["color"];
+        // $pluralnoun =$_GET["pluralnoun"];
+        // $celebrity =$_GET["celebrity"];
+        // echo "Roses are $color <br>";
+        // echo "$pluralnoun are blue <br>";
+        // echo "I love $celebrity <br>";
+
+        // $numbers = array("jay","sai","teja","Mani");
+        // $numbers[4]= "bojja";
+        // echo $numbers[4];
+        // echo "<br>" .count($numbers) . "</br>";
+
+        // $fruit = $_POST["fruit"];
+        // echo $fruit[0];
+
+
+        $CGPA = array("jim"=>7.6,"pam"=>7.9,"oscar"=>9.0);
+            echo $CGPA[$_POST["student"]];
+        // echo "<pre>" . print_r($CGPA,true) . "</pre>";
+
+
+
+
+
+
+
+
+
+
+
+
+
+        ?>  
+    </body>
+</html>
